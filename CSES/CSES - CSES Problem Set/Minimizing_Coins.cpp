53b0ff23b26b2d1a5279c2eea85d0d2499da41ae
Tyrone Matthew Phanghegar <tyrone.matthew2002@gmail.com>
2022-11-11 23:58:54 +0700

    Added a bunch of practice problems from CSES and SPOJ

---
+++ CSES/CSES - CSES Problem Set/Minimizing_Coins.cpp
@@ -0,0 +1,48 @@
+//  Minimizing Coins
+
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+#define pb push_back
+#define mp make_pair
+#define MOD 1000000007
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+void precompute() {
+
+}
+
+void work() {
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> money(n);
+    for ( int i = 0 ; i < n ; i++ ) {
+        cin >> money[i];
+    }
+    vector<ll> dp(k+1, INT_MAX);
+    dp[0] = 0;
+    for ( int i = 1 ; i <= k ; i++ ) {
+        for ( int j = 0 ; j < n ; j++ ) {
+            if ( i - money[j] >= 0 ) {
+                dp[i] = min(dp[i], dp[i-money[j]] + 1);
+            }
+        }
+    }
+    if ( dp[k] >= INT_MAX ) {
+        cout << -1 << endl;
+    } else {
+        cout << dp[k] << endl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll t = 1;
+//    cin >> t;
+    while ( t-- ) work();
+    return 0;
+}
